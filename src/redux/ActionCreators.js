775ddd1a42ee80622a452b2d7d377b22097399b2
Alex Kim <312akim@gmail.com>
2020-05-13 01:00:21 -0500

    Redux Thunk

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from "./ActionTypes"; // * imports all named exports
+import { CAMPSITES } from "../shared/campsites";
 
 export const addComment = (campsiteId, rating, author, text) => ({ //Action Creator Function
     type: ActionTypes.ADD_COMMENT,
@@ -8,4 +9,27 @@ export const addComment = (campsiteId, rating, author, text) => ({ //Action Crea
         author: author,
         text: text
     }
+});
+
+export const fetchCampsites = () => dispatch => { //Redux Thunk Syntax
+
+    dispatch(campsitesLoading());
+
+    setTimeout(() => {
+        dispatch(addCampsites(CAMPSITES));
+    }, 2000);
+};
+
+export const campsitesLoading = () => ({
+    type: ActionTypes.CAMPSITES_LOADING
+});
+
+export const campsitesFailed = errMess => ({
+    type: ActionTypes.CAMPSITES_FAILED,
+    payload: errMess
+});
+
+export const addCampsites = campsites => ({
+    type: ActionTypes.ADD_CAMPSITES,
+    payload: campsites
 });
