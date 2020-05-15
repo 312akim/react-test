aec7f2d0f22682d823371a893e166c9aa8e62432
Alex Kim <312akim@gmail.com>
2020-05-15 02:06:24 -0500

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -26,7 +26,7 @@ class CommentForm extends Component {
 
     handleSubmit = (values) => {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.fullName, values.comment);
+        this.props.postComment(this.props.campsiteId, values.rating, values.fullName, values.comment);
     };
 
     render() {
@@ -125,7 +125,7 @@ function RenderCampsite({ campsite }) {
     );
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col md-5 m-1">
@@ -142,7 +142,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
                         }).format(new Date(Date.parse(comment.date)))}
                     </div>
                 ))}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
     }
@@ -190,7 +190,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments 
                         comments={props.comments} 
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                 </div>
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,15 +1,6 @@
 import * as ActionTypes from "./ActionTypes"; // * imports all named exports
 import { baseUrl } from "../shared/baseUrl";
 
-export const addComment = (campsiteId, rating, author, text) => ({ //Action Creator Function
-    type: ActionTypes.ADD_COMMENT,
-    payload: {
-        campsiteId: campsiteId,
-        rating: rating,
-        author: author,
-        text: text
-    }
-});
 
 export const fetchCampsites = () => dispatch => { //Redux Thunk Syntax
 
@@ -77,6 +68,46 @@ export const addComments = comments => ({
     type: ActionTypes.ADD_COMMENTS,
     payload: comments
 });
+
+export const addComment = comment => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
+});
+
+export const postComment = (campsiteId, rating, author, text) => dispatch => { // Action Creator using thunk middleware
+    const newComment = {
+        campsiteId: campsiteId,
+        rating: rating,
+        author: author,
+        text: text
+    };
+    newComment.date = new Date ().toISOString();
+
+    return fetch(baseUrl + "comments", {
+            method: "POST",
+            body: JSON.stringify(newComment),
+            headers: {
+                "Content-Type": "application/json" // so Server knows to expect the body to be JSON
+            }
+            })
+            .then(response => { //Receiving any response back resolves promise as fulfilled. Response.ok checks for status code in successful range 200-299
+                    if (response.ok) {
+                        return response;
+                    } else {
+                        const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                        error.response = response;
+                        throw error;
+                    }
+            },
+            error => { throw error; }
+        )
+        .then(response => response.json())
+        .then(response => dispatch(addComment(response)))
+        .catch(error => {
+            console.log("post comment", error.message);
+            alert("Your comment could not be posted\nError: " + error.message);
+        });
+};
 
 export const fetchPromotions = () => dispatch => { 
 
